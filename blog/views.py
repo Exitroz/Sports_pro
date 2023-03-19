a1bc3ba4cfd2e4c33044b1c74976e0314d5e7841
Great Special <sirgreatspecial@gmail.com>
2023-03-19 14:41:41 +0100

    added blog models and views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+
+from .forms import BlogPostForm
+from .models import BlogPostModel
 
 # Create your views here.
+
+
+def add_blog(request):
+    
+    if request.method == 'POST' : # and request.FILES['image']
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog_obj = form.save(commit=False)
+            blog_obj.user = request.user
+            
+            blog_obj.save()  
+        return redirect('/') # see_blog
+    else:
+        form = BlogPostForm()
+ 
+    context = {'form': form}
+    template_name = ''
+    return render(request, template_name, context)
+
+
+def blog_detail(request, slug):
+    template_name = ''
+    context = {}
+    try:
+        blog_obj = get_object_or_404(BlogPostModel, slug=slug)
+        context['blog_obj'] = blog_obj
+    except Exception as e:
+        raise e
+    return render(request, template_name, context)
