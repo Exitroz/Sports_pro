a9f2948ab32a706775375a5f742331a458a8c9c5
Henry <ezeobih@gmail.com>
2023-03-20 07:43:48 +0100

    static files

--- sports_pro/urls.py
+++ sports_pro/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('soccer.urls')),
+    path('basketball/', include('basketball.urls')),
+    # path('blog/', include('blog.urls')),
+    path('predictions/', include('predictions.urls')),
+    
 ]
