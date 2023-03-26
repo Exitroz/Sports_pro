38e44c1406cbb09d17903da4b31ceb4bc817a02b
Great Special <sirgreatspecial@gmail.com>
2023-03-26 19:39:44 +0100

    made more adjustment

--- home/views.py
+++ home/views.py
@@ -1,32 +1,51 @@
 from django.shortcuts import render
 import requests
+import datetime
 from django.http import JsonResponse
 # Create your views here.
 
 
-def index(request):
-    url = "https://livescore6.p.rapidapi.com/matches/v2/list-live"
-
-    querystring = {"Category":"soccer","Timezone":"-7"}
+def index(request):    
+    url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-league"
+    
+    query_string_premier_league = {"Category":"soccer","Ccd":"england","Scd":"premier-league","Timezone":"-7"}
+    querystring = {"Category":"soccer","Ccd":"england","Scd":"premier-league","Timezone":"-7"}
 
     headers = {
-        "X-RapidAPI-Key": "bb17079e36msh74bf5d47086ca7ep13fc06jsnc033492ad114",
+        "X-RapidAPI-Key": "d31ffed9cdmshdd3b46d49113fffp17b050jsn36beb1a72767",
         "X-RapidAPI-Host": "livescore6.p.rapidapi.com"
     }
 
+    response = requests.request('GET', url, headers=headers, params=querystring)
+
     
-    response = requests.get(url, headers=headers, params=querystring)
-    
+    # response = requests.get(url, headers=headers, params=querystring)
+    print(response.status_code)
     # Check if the API call was successful
     if response.status_code == 200:
         # Convert the response content to JSON
         data = response.json()
-        # Return the data as a JSON response to the frontend
-        jsonResponse = JsonResponse(data, safe=False)
-        # return jsonResponse
+        
+        stages = data['Stages']
+        
+        for stage in stages:
+            print(stage.keys())
+            
+            table = stage['LeagueTable']['L'][0]['Tables']
+            
+            for row in table:
+                print(row.keys())
+                print(row.values())
+            
+        context = {
+            'stages':stages,
+        }
+        
+        return render(request, 'home/index.html', context)
+
     else:
         # If the API call failed, return the error message as a JSON response
         error_message = {'error': response.reason}
         return JsonResponse(error_message, status=response.status_code)
     
-    return render(request, 'home/index-4.html', {'jsonResponse': jsonResponse})
+    return render(request, 'home/index.html', {'jsonResponse': jsonResponse})
--- soccer/views.py
+++ soccer/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
 import requests
+import datetime
 from django.http import JsonResponse
 # Create your views here.
 
 def index(request):
+    template_name = 'soccer.html'
+    
+    url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-league"
+
+    query_string_premier_league = {"Category":"soccer","Ccd":"england","Scd":"premier-league","Timezone":"-7"}
+
+    headers = {
+        "X-RapidAPI-Key": "d31ffed9cdmshdd3b46d49113fffp17b050jsn36beb1a72767",
+        "X-RapidAPI-Host": "livescore6.p.rapidapi.com"
+    }
+
+    
+    response = requests.get(url, headers=headers, params=query_string_premier_league)
+    
+    # Check if the API call was successful
+    if response.status_code == 200:
+        # Convert the response content to JSON
+        data = response.json()
+        
+        stages = data['Stages']
+        
+        context = {
+            'stages':stages,
+        }
+        return render(request, template_name, context)
+        # Return the data as a JSON response to the frontend
+    else:
+        # If the API call failed, return the error message as a JSON response
+        print('failed')
+        error_message = {'error': response.reason}
+        return JsonResponse(error_message, status=response.status_code)
+    
+
+
+def live(request):
     url = "https://livescore6.p.rapidapi.com/matches/v2/list-live"
 
     querystring = {"Category":"soccer","Timezone":"-7"}
@@ -27,9 +63,6 @@ def index(request):
             'stages':stages,
         }
         return render(request, template_name, context)
-        # Return the data as a JSON response to the frontend
-        jsonResponse = JsonResponse(data, safe=False)
-        # return jsonResponse
     else:
         # If the API call failed, return the error message as a JSON response
         error_message = {'error': response.reason}
@@ -39,35 +72,29 @@ def index(request):
 
 
 
+def fixturesByDate(request, date: str):
+    template_name = 'fixtures.html'
+    dt = datetime.datetime.now()
+    print(dt)
 
+    url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-date"
 
+    querystring = {"Category":"soccer","Date":date,"Timezone":"-7"}
 
+    headers = {
+        "X-RapidAPI-Key": "d31ffed9cdmshdd3b46d49113fffp17b050jsn36beb1a72767",
+        "X-RapidAPI-Host": "livescore6.p.rapidapi.com"
+    }
 
+    response = requests.get(url, headers=headers, params=querystring)
 
-# def get_livescore(request):
-#     # Replace with your LiveScore API endpoint URL and API key
-#     endpoint = 'https://livescore-api.com/api-client/scores/live.json'
-#     api_key = 'YOUR_API_KEY'
-
-#     # Set optional parameters for the API call
-#     params = {
-#         'key': api_key,
-#         'secret': 'YOUR_API_SECRET',
-#         'countries': 'england',
-#         'leagues': 'premier-league',
-#         'timezone': 'Europe/London'
-#     }
-
-#     # Make a GET request to the API endpoint using requests library
-#     response = requests.get(endpoint, params=params)
+    data = response.json()
+    stages = data['Stages']
     
-#     # Check if the API call was successful
-#     if response.status_code == 200:
-#         # Convert the response content to JSON
-#         data = response.json()
-#         # Return the data as a JSON response to the frontend
-#         return JsonResponse(data)
-#     else:
-#         # If the API call failed, return the error message as a JSON response
-#         error_message = {'error': response.reason}
-#         return JsonResponse(error_message, status=response.status_code)
+    print(response.text)
+    
+    context = {
+        'stages':stages
+    }
+    
+    return render(request, template_name, context)
