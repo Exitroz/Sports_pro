4f05e5c6a868bdb1cc891f6f11572fde99a3460f
Great Special <sirgreatspecial@gmail.com>
2023-04-01 17:34:16 +0100

    added competions

--- home/views.py
+++ home/views.py
@@ -5,7 +5,9 @@ from django.http import JsonResponse
 # Create your views here.
 
 
-def index(request):    
+def index(request):  
+    dt = datetime.datetime.now()
+    Fixtures = []  
     url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-league"
     
     querystring = {"Category":"soccer","Ccd":"england","Scd":"premier-league","Timezone":"-7"}
@@ -27,17 +29,29 @@ def index(request):
         
         stages = data['Stages']
         
-        # for stage in stages:
-        #     print(stage.keys())
-            
-        #     table = stage['LeagueTable']['L'][0]['Tables']
-            
-        #     for row in table:
-        #         print(row.keys())
-        #         print(row.values())
+        for stage in stages:
+            # print(stage.keys())
+            competion_name = stage['CompN']
+            events = stage['Events']
+            print('events', len(events))
+            for row in events:
+                # print(row.keys())
+                # print(row['Esd'])
+                event_date = row['Esd']
+                event_year = int(str(event_date)[0:4])
+                event_month = int(str(event_date)[4:6])
+                event_day = int(str(event_date)[6:8])
+                # print(event_year, 'ED',event_day, 'DTD',dt.day, 'EM',event_month, 'DTM',dt.month)
+                
+                if  event_year >= dt.year and event_month == dt.month:
+                    if not event_day < dt.day:
+                        Fixtures.append(row)
+                elif event_year >= dt.year and event_month > dt.month:
+                    Fixtures.append(row)
             
         context = {
             'stages':stages,
+            'fixtures':Fixtures[0:25],
         }
         
         return render(request, 'home/index.html', context)
--- soccer/views.py
+++ soccer/views.py
@@ -30,7 +30,6 @@ def index(request):
         
         stages = data['Stages']
         
-        stages = data['Stages']
         
         for stage in stages:
             # print(stage.keys())
@@ -65,6 +64,79 @@ def index(request):
         error_message = {'error': response.reason}
         return JsonResponse(error_message, status=response.status_code)
     
+
+def competion_events(request, league: str, stage: str):
+    Results = []
+    Fixtures = []
+    template_name = 'competions.html'
+    
+    dt = datetime.datetime.now()
+    print(dt.strftime('%Y-%m-%d'))
+    datefmt = str(dt.year)+str(dt.month)+str(dt.day)
+    
+    url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-league"
+
+    query_string = {"Category":"soccer","Ccd":league,"Scd":stage,"Timezone":"-7"}
+
+    headers = {
+        "X-RapidAPI-Key": "6b988f2637msh4473b4fe6cd4359p151307jsn6639db590b7a",
+        "X-RapidAPI-Host": "livescore6.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=query_string)
+    
+    # Check if the API call was successful
+    if response.status_code == 200:
+        # Convert the response content to JSON
+        data = response.json()
+        
+        stages = data['Stages']
+        
+        for stage in stages:
+            # print(stage.keys())
+            competion_name = stage['CompN']
+            stage_name = stage['Snm']
+            events = stage['Events']
+            print('events', len(events))
+            for row in events:
+                # print(row.keys())
+                # print(row['Esd'])
+                event_date = row['Esd']
+                event_year = int(str(event_date)[0:4])
+                event_month = int(str(event_date)[4:6])
+                event_day = int(str(event_date)[6:8])
+                # print(event_year, 'ED',event_day, 'DTD',dt.day, 'EM',event_month, 'DTM',dt.month)
+                
+                if event_year < dt.year:
+                    Results.append(row)
+                elif event_year == dt.year and event_month < dt.month:
+                    Results.append(row)
+                elif event_year == dt.year and event_month == dt.month:
+                    if event_day < dt.day:
+                        Results.append(row)
+                    else:
+                        Fixtures.append(row) 
+                elif  event_year >= dt.year and event_month > dt.month:
+                    Fixtures.append(row)
+                    
+        print('Fixtures', len(Fixtures))
+        print('Results', len(Results))
+
+        context={
+            'stages':stages,
+            'Fixtures':Fixtures,
+            'Results':Results,
+            'Competion_name':competion_name,
+            'stage_name':stage_name,
+        }
+    else:
+        context={
+            'Fixtures':Fixtures,
+            'Results':Results,
+            'Competion_name':"Null",
+            'stage_name':"Null",
+        }
+    return render(request, template_name, context)
 
 
 
