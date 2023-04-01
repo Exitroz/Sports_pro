c67ecf3ffa6cba8b4824d6cdb5d9372651002c30
Henry <ezeobih@gmail.com>
2023-04-01 18:42:21 +0100

    raw

--- soccer/urls.py
+++ soccer/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index, name='soccer'),
-    path('live/', views.live, name='live'),
-    path('fixtures/<str:date>/', views.fixturesByDate, name='fixtures'),
-    # path('my-url/', views.my_view, name='my-url'),
+    
 ]
