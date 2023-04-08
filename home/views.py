b757d8e086a4e07785ded2cdb88b76b3ca00a0c5
Great Special <sirgreatspecial@gmail.com>
2023-04-08 10:36:05 +0100

    done with home and soccer apss

--- home/views.py
+++ home/views.py
@@ -5,12 +5,18 @@ from django.http import JsonResponse
 # Create your views here.
 
 
-def index(request):  
+def index(request):  # On the homepage the current day fixtures and results, group by competions
     dt = datetime.datetime.now()
-    Fixtures = []  
-    url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-league"
+    print(dt.strftime('%Y-%m-%d'))
+    datefmt = str(dt.year)+str(dt.month)+str(dt.day)
     
+    Fixtures = []  
+    
+    url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-league"    
+    today_url = "https://livescore6.p.rapidapi.com/matches/v2/list-by-date"
+        
     querystring = {"Category":"soccer","Ccd":"england","Scd":"premier-league","Timezone":"-7"}
+    today_querystring = {"Category":"soccer","Date":datefmt,"Timezone":"-7"}
 
     headers = {
         "X-RapidAPI-Key": "6b988f2637msh4473b4fe6cd4359p151307jsn6639db590b7a",
@@ -19,15 +25,19 @@ def index(request):
 
     response = requests.request('GET', url, headers=headers, params=querystring)
 
+    today_response = requests.get(today_url, headers=headers, params=today_querystring)
     
     # response = requests.get(url, headers=headers, params=querystring)
     print(response.status_code)
     # Check if the API call was successful
-    if response.status_code == 200:
+    if response.status_code == 200 or today_response.status_code == 200:
         # Convert the response content to JSON
-        data = response.json()
         
+        data = response.json()
         stages = data['Stages']
+        
+        t_data = today_response.json()
+        today_data = t_data['Stages']
         
         for stage in stages:
             # print(stage.keys())
@@ -43,19 +53,16 @@ def index(request):
                 event_day = int(str(event_date)[6:8])
                 # print(event_year, 'ED',event_day, 'DTD',dt.day, 'EM',event_month, 'DTM',dt.month)
                 
-                if  event_year >= dt.year and event_month == dt.month:
-                    if not event_day < dt.day:
-                        Fixtures.append(row)
-                elif event_year >= dt.year and event_month > dt.month:
+                if  event_year >= dt.year and event_month == dt.month :
                     Fixtures.append(row)
             
         context = {
             'stages':stages,
+            'today_data':today_data,
             'fixtures':Fixtures[0:25],
         }
         
-        return render(request, 'home/index.html', context)
-
+        return render(request, 'home/index.html', context)        
     else:
         # If the API call failed, return the error message as a JSON response
         error_message = {'error': response.reason}
