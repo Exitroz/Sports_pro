0e00c6542167e6a30c5b34eea5e655205a429036
Henry <ezeobih@gmail.com>
2023-04-14 15:52:27 +0100

    navbar update

--- soccer/views.py
+++ soccer/views.py
@@ -16,7 +16,8 @@ def index(request):
     query_string_premier_league = {"Category":"soccer","Ccd":"england","Scd":"premier-league","Timezone":"-7"}
 
     headers = {
-        "X-RapidAPI-Key": "6b988f2637msh4473b4fe6cd4359p151307jsn6639db590b7a",
+        # "X-RapidAPI-Key": "6b988f2637msh4473b4fe6cd4359p151307jsn6639db590b7a",
+        'X-RapidAPI-Key': 'bb17079e36msh74bf5d47086ca7ep13fc06jsnc033492ad114',
         "X-RapidAPI-Host": "livescore6.p.rapidapi.com"
     }
 
@@ -200,8 +201,8 @@ def league_events(request, country: str, league: str):
                 elif  event_year >= dt.year and event_month > dt.month:
                     Fixtures.append(row)
                     
-        print('Fixtures', len(Fixtures))
-        print('Results', len(Results))
+        #print('Fixtures', len(Fixtures))
+       # print('Results', len(Results))
 
         context={
             'stages':stages,
